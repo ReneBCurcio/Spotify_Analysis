63ebd3dda94657ff4653f4c35b1fe67e431a3bd5
ReneBCurcio <renecbastos@outlook.com>
2023-12-09 17:26:52 -0300

    Add format for Analysis

--- Spotify_Analysis.py
+++ Spotify_Analysis.py
@@ -9,13 +9,17 @@ st.set_page_config(
 
 df= pd.read_csv("01 Spotify.csv")
 
-artist = st.selectbox("Artista", df["Artist"].value_counts().index)
+artist = st.sidebar.selectbox("Artista", df["Artist"].value_counts().index)
 
 df.set_index("Track", inplace=True)
 
 #st.line_chart(df[df["Stream"] > 10000000]["Stream"])
 
 df_Artista = df[df["Artist"] == artist]  
-album = st.selectbox("Album", df_Artista["Album"].value_counts().index)
+album = st.sidebar.selectbox("Album", df_Artista["Album"].value_counts().index)
 df_Album = df[df["Album"] == album] 
-st.bar_chart(df_Album[df_Album["Stream"] > 10000000]["Stream"])
+
+
+col1, col2 = st.columns(2)
+col1.bar_chart(df_Album[df_Album["Stream"] > 10000000]["Stream"])
+col2.line_chart(df_Album[df_Album["Stream"] > 10000000]["Danceability"])
