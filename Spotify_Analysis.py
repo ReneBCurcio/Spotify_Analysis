4aa257f7fab5fe2d3143a1f7251802fcc8f729f1
ReneBCurcio <renecbastos@outlook.com>
2023-12-09 17:10:19 -0300

    Add selectbox for Analysis

--- Spotify_Analysis.py
+++ Spotify_Analysis.py
@@ -2,5 +2,20 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 
-df = pd.read_csv("01 Spotify.csv")
-df
+st.set_page_config(
+        layout="wide",
+        page_title="Spotify Songs"
+)
+
+df= pd.read_csv("01 Spotify.csv")
+
+artist = st.selectbox("Artista", df["Artist"].value_counts().index)
+
+df.set_index("Track", inplace=True)
+
+#st.line_chart(df[df["Stream"] > 10000000]["Stream"])
+
+df_Artista = df[df["Artist"] == artist]  
+album = st.selectbox("Album", df_Artista["Album"].value_counts().index)
+df_Album = df[df["Album"] == album] 
+st.bar_chart(df_Album[df_Album["Stream"] > 10000000]["Stream"])
